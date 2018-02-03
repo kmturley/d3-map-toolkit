3c0181b101ead410b872bd01bc47c790adf6c559
Kim Turley <kim@bynd.com>
2018-02-03 16:10:50 -0500

    Multiple level resolution maps

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,13 @@
 /* map */
 
-var folder = window.location.hash ? window.location.hash.slice(1) : 'countries',
+var isReset = true,
+  folder = 'countries',
   width = window.innerWidth,
   height = window.innerHeight,
   active = d3.select(null),
   scaleMin = 1,
-  scaleMax = 10,
+  scaleMax = 50,
+  currentLayer = null,
   overlay = document.getElementById('overlay');
 
 var projection = d3.geoMercator()
@@ -33,26 +35,37 @@ svg
   .attr("class", "sea")
   .attr("width", width)
   .attr("height", height)
-  .on("click", reset);
+  .on("click", resetting);
 
 var g = svg.append("g");
 svg.call(zoom);
 
-d3.json("json/" + folder + "/6.toposimplify.json", function(error, map) {
-  if (error) throw error;
-  g
-    .selectAll("path")
-    .data(topojson.feature(map, map.objects.layer).features)
-    .enter()
-    .append("path")
-    .attr("d", path)
-    .attr("class", "feature")
-    .on("click", clicked);
-  g.style("stroke-width", ".5");
-});
+window.addEventListener("hashchange", load, false);
+
+function load() {
+  folder = window.location.hash ? window.location.hash.slice(1) : 'countries';
+  d3.json("json/" + folder + "/6.toposimplify.json", function(error, map) {
+    if (error) throw error;
+    if (currentLayer) {
+      currentLayer.remove();
+    }
+    console.log("load", "json/" + folder + "/6.toposimplify.json", map.objects.layer.geometries.length);
+    currentLayer = g
+      .selectAll("path")
+      .data(topojson.feature(map, map.objects.layer).features)
+      .enter()
+      .append("path")
+      .attr("d", path)
+      .attr("class", "feature")
+      .on("click", clicked);
+    if (!isReset) {
+      reset();
+    }
+  });
+}
 
 function clicked(d) {
-  if (active.node() === this) return reset();
+  if (active.node() === this) return resetting();
   active.classed("active", false);
   active = d3.select(this).classed("active", true);
 
@@ -67,20 +80,33 @@ function clicked(d) {
   svg
     .transition()
     .duration(750)
-    .call(zoom.transform, d3.zoomIdentity.translate(translate[0], translate[1]).scale(scale));
+    .call(zoom.transform, d3.zoomIdentity.translate(translate[0], translate[1]).scale(scale))
+    .on('end', function () {
+      if (folder === 'countries') {
+        window.location.hash = 'states';
+      }
+    });
 
   overlay.innerHTML = (d.properties.name || '') + ( ' (' + d.id + ')' || '');
   console.log('clicked', d.id, d.properties.name);
 }
 
+function resetting() {
+  isReset = false;
+  if (folder === 'states') {
+    window.location.hash = 'countries';
+    overlay.innerHTML = 'Click a country';
+  }
+}
+
 function reset() {
+  isReset = true;
   active.classed("active", false);
   active = d3.select(null);
   svg
     .transition()
     .duration(750)
     .call(zoom.transform, d3.zoomIdentity);
-    overlay.innerHTML = 'Click a country';
 }
 
 function zoomed() {
@@ -91,3 +117,5 @@ function zoomed() {
 function stopped() {
   if (d3.event.defaultPrevented) d3.event.stopPropagation();
 }
+
+load();
